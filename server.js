1b697c7d240da10a04c8785d52712d1cd015beb5
Rishabh Saxena <orishabh1@gmail.com>
2017-03-24 10:04:21 +0530

    add twitter keys

--- server.js
+++ server.js
@@ -22,7 +22,29 @@ app.use(flash());
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 
+var Twitter = require('twitter');
 
+var client = new Twitter({
+  consumer_key: 'I1fumJtHqLRwLc4oPzqkghqoD',
+  consumer_secret: 'J7vMfxXVI4zZyXqFtoLPV2fwyUQWVP8Dh1Ky6PBgFUAZd4xD0C',
+  access_token_key: '3017462262-3bJEiTDm87TXcPHVD6CSTADSEZR7ngpfw12iUNH',
+  access_token_secret: '9f7Nn35SD3Qbr4eKdjVzM2YukJiFK7jt4PB4pu4t6Vjfq'
+});
+
+/*var config = {
+    consumerKey: 'I1fumJtHqLRwLc4oPzqkghqoD',
+    consumerSecret: 'J7vMfxXVI4zZyXqFtoLPV2fwyUQWVP8Dh1Ky6PBgFUAZd4xD0C',
+    accessToken: '3017462262-3bJEiTDm87TXcPHVD6CSTADSEZR7ngpfw12iUNH',
+    accessTokenSecret: '10f7Nn35SD3Qbr4eKdjVzM2YukJiFK7jt4PB4pu4t6Vjfq',
+    callBackUrl: 'http://localhost:8080/auth/twitter/callback'
+};*/
+
+var params = {screen_name: 'royalharsh95'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets[0].created_at);
+  }
+});
 
 app.get('/', function(req, res) {
 	res.render('index.ejs');
